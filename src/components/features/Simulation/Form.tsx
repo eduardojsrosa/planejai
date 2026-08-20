2cda8b09e98ca82de63d4d9d1f23d23dbd709b29
eduardo.rosa <eduardo.rosa@diferpan.com.br>
2026-08-20 17:18:48 -0300

    Configurando os Dados do Formulário Multi-Step

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,21 +1,15 @@
-import { PiggyBank } from "lucide-react"
 import { FormStep } from "./FormStep"
 import { StepProgress } from "./Progress"
 
+import { simulationFormSteps } from '@/data/simulation'
+
 export const SimulattionForm = () => {
+  const currentStep = simulationFormSteps[0]
+
   return (
     <>
       <StepProgress currentStep={1} totalSteps={5} />
-      <FormStep 
-        icon={PiggyBank}
-        title="Renda mensal bruta"
-        question="Quanto é depositado na sua conta todo mês?" 
-        inputProps={{
-          type: "text",
-          placeholder: "5.000,00",
-          prefix: "R$",
-        }}      
-      />
+      <FormStep key={currentStep.id} {...currentStep} />
     </>
   )
 }
