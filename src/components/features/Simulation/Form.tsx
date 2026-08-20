34b92776b89e100e3020d8843a5f72fed99d73da
eduardo.rosa <eduardo.rosa@diferpan.com.br>
2026-08-20 17:15:02 -0300

    Interface do Formulário de Simulação

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,9 +1,21 @@
+import { PiggyBank } from "lucide-react"
+import { FormStep } from "./FormStep"
 import { StepProgress } from "./Progress"
 
 export const SimulattionForm = () => {
   return (
     <>
       <StepProgress currentStep={1} totalSteps={5} />
+      <FormStep 
+        icon={PiggyBank}
+        title="Renda mensal bruta"
+        question="Quanto é depositado na sua conta todo mês?" 
+        inputProps={{
+          type: "text",
+          placeholder: "5.000,00",
+          prefix: "R$",
+        }}      
+      />
     </>
   )
 }
